b3fc82f35149acbf7dda035677cb84cd26a7699f
Faduma Elmi <fowziiae@gmail.com>
2021-02-18 13:17:49 -0600

    added post route

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,22 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+// POST Route
+router.post('/', (req, res) => {
+  const item = req.body;
+  const sqlText = `INSERT INTO "shopping_list" ("name", "quantity", "unit" ) 
+                   VALUES ($1, $2, $3)`;
+
+  pool
+    .query(sqlText, [item.name, item.quantity, item.unit, item.isPurchased])
+    .then((result) => {
+      console.log(`Added item to the shopping list`, item);
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(`Error making database query ${sqlText}`, error);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
-
-import Header from '../Header/Header.jsx'
+import axios from 'axios';
+import Header from '../Header/Header.jsx';
 import './App.css';
-
+import { useState, useEffect } from 'react';
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <main>
-                <p>Under Construction...</p>
-            </main>
-        </div>
-    );
+  let [newItemName, setNewItemName] = useState('');
+  let [newQuantity, setNewQuantity] = useState('');
+  let [newUnit, setNewUnit] = useState('');
+
+  //POST route
+  const addItem = () => {
+    axios
+      .post('/list', {
+        name: newItemName,
+        quantity: newQuantity,
+        unit: newUnit,
+      
+      .then((response) => {
+        fetchList();
+      })
+      .catch((err) => {
+        alert('Error Adding item');
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <p>Under Construction...</p>
+      </main>
+    </div>
+  );
 }
 
 export default App;
