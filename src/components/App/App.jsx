5ef12f05af6e138023e1d5df386da383c9e68c25
McKynlee Westman <mckynlee.westman@gmail.com>
2021-02-18 10:22:42 -0600

    App-side GET set up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
 
+// Import useEffect to auto-render GET on DOM on page load:
+import { useEffect, useState } from 'react';
+
+// import axios:
+import axios from 'axios';
+
+// import components:
 import Header from '../Header/Header.jsx'
 import './App.css';
 
-
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <main>
-                <p>Under Construction...</p>
-            </main>
-        </div>
-    );
+
+  // auto-render db table info on DOM:
+  useEffect(() => {
+    fetchList();
+  }, []); //end useEffect
+
+  const fetchList = () => {
+    axios.get('/list')
+      .then(response => {
+        console.log('get response:', response.data);
+      })
+      .catch(error => {
+        console.log('GET error:', error);
+      })
+  }// end fetchList
+
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <p>Under Construction...</p>
+      </main>
+    </div>
+  );
 }
 
 export default App;
