9440b4f2cb9c9b64612e41be3a054da4b98869ba
William Krug <william.p.krug@gmail.com>
2021-02-18 10:45:12 -0600

    css styling for list comonent

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -2,11 +2,24 @@ import './ShoppingList.css';
 
 function ShoppingList() {
   return (
-    <div>
+    <div className="shopping-list">
       <h2>Shopping List</h2>
       {/* Button Controls */}
-      <button>Reset</button>
-      <button>Clear</button>
+      <div className="button-controls">
+        <button className="item-button">Reset</button>
+        <button className="item-button">Clear</button>
+      </div>
+      {/* List Items */}
+      <div>
+        <div className="list-item">
+          <p>Apples</p>
+          <p>5 lbs</p>
+          <div className="item-buttons">
+            <button>Buy</button>
+            <button>Remove</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
