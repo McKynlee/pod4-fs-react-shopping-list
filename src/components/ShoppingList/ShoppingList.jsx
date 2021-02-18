da15dfa5e1b98c30ded4253b64b581bf82581a7e
William Krug <william.p.krug@gmail.com>
2021-02-18 10:22:43 -0600

    linked css file to shopping list component

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,5 +1,14 @@
+import './ShoppingList.css';
+
 function ShoppingList() {
-  return;
+  return (
+    <div>
+      <h2>Shopping List</h2>
+      {/* Button Controls */}
+      <button>Reset</button>
+      <button>Clear</button>
+    </div>
+  );
 }
 
 export default ShoppingList;
